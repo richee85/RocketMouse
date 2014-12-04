1baa2d7d74d499f99de7f4f003e2f3418c0e5251
richee85 <RyU@richeemacbook.local>
2014-12-04 22:37:31 +0100

    initial commit

---
+++ Assets/GameUIManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameUIManager : MonoBehaviour {
+
+	bool isPaused = false;
+
+	public void Restart()
+	{
+		Application.LoadLevel("RocketMouse");
+	}
+	public void Quit() 
+	{
+		Application.LoadLevel("MenuScene");
+	}
+	public void pause()
+	{
+		isPaused = !isPaused;
+		if (isPaused) Time.timeScale = 0;
+		else Time.timeScale = 1;
+	}
+}
